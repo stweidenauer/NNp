205e35dc7275983072241aa66fc3137ff7f5afeb
Steffen <stweidenauer@gmail.com>
2021-10-22 18:23:14 +0200

    end of chapter 3 Nnp and end of part 1 CS

--- Matplotlib_CS/part_1.py
+++ Matplotlib_CS/part_1.py
@@ -1,19 +1,28 @@
 from matplotlib import pyplot as plt
 
+# show all styles
+# print(plt.style.available)
+# plt.style.use("tableau-colorblind10")
+plt.xkcd()
+
 # Median Developer Salaries by Age
 ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 dev_y = [38496, 42000, 46752, 49320, 53200,
          56000, 62316, 64928, 67317, 68748, 73752]
-
-plt.plot(ages_x, dev_y, label='All Devs')
+# after axes format string possible, barely readable
+plt.plot(ages_x, dev_y, '1k--', label='All Devs')
 
 # Median Python Developer Salaries by Age
 py_dev_y = [45372, 48876, 53850, 57287, 63016,
             65998, 70003, 70000, 71496, 75370, 83640]
+# explicit as arguments
+plt.plot(ages_x, py_dev_y, color='#5a7d9a', linestyle="-", marker='o', label='Python')
 
-
-plt.plot(ages_x, py_dev_y, label='Python')
+# Median JavaScript Developer Salaries by Age
+js_dev_y = [37810, 43515, 46823, 49293, 53437,
+            56373, 62375, 66674, 68745, 68746, 74583]
+plt.plot(ages_x, js_dev_y, label='JavaScript')
 
 
 # name for axes and title
@@ -26,5 +35,14 @@ plt.title('Median Salary (USD) by age')
 # ex: plt.legend(['AllDevs', 'Python'])
 # better is adding label directly to the plot and call legend empty
 plt.legend()
+
+# show a grid, if not included in style
+# plt.grid(True)
+
+# fixes the padding
+plt.tight_layout()
+
+# save as png
+plt-plt.savefig('plot.png')
 
 plt.show()
